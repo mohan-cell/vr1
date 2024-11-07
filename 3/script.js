2841fcc405f6b5d288e39f776809e5908070b741
mohan-cell <57553522+mohan-cell@users.noreply.github.com>
2024-11-07 09:50:30 +0530

    Delete 3 directory

--- 3/script.js
+++
@@ -1,7 +0,0 @@
-// Change the second argument to your options:
-// https://github.com/sampotts/plyr/#options
-          var player = new Plyr('.container video', {
-            muted: false,
-            volume: 1,
-            controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
-          });
